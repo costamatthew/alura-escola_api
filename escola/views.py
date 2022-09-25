e633929433a9bbaf984a3b7d0f13c99d97b93548
Mateus Costa <mateusdsilvacosta@outlook.com>
2022-09-24 21:45:38 -0300

    escola

---
+++ escola/views.py
@@ -0,0 +1,13 @@
+from rest_framework import viewsets
+from escola.models import Aluno, Curso
+from serializer import AlunoSerializer, CursoSerializer
+
+
+class AlunosViewSet(viewsets.ModelViewSet):
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+
+class CursosViewSet(viewsets.ModelViewSet):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
